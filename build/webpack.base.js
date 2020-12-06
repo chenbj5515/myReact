7d679ede4745815d3da6597e7d5329a6c49444db
chenbj <chenbaijin@baijiahulian.com>
2020-12-07 00:36:07 +0800

    feat: 第一阶段完成，实现了利用babel将JSX转为了目标对象;

---
+++ build/webpack.base.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        bundle: path.resolve(__dirname, '../src/stage-0/index.js'),
+    },
+    resolve: {
+        extensions: [],
+        alias: {
+        },
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|mjs|jsx|ts|tsx)$/,
+                include: path.resolve(__dirname, '../src'),
+                exclude: /node_modules/,
+                loader: require.resolve('babel-loader'),
+                options: {
+                    presets: [
+                        [
+                            '@babel/preset-env',
+                        ],
+                        // jsx支持
+                        '@babel/preset-react',
+                    ],
+                    plugins: [
+                    ],
+                    cacheDirectory: true,
+                    cacheCompression: true,
+                    compact: true,
+                },
+            }
+        ],
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+    ],
+    externals: {
+    }
+};
---
+++ src/stage-0/index.js
@@ -0,0 +1,33 @@
+const isType = type => target => Object.prototype.toString.call(target) === `[object ${type}]`;
+
+const transformChild = child => isType('Object')(child) ? child : createTextElement(child);
+
+const createElement = (type, props, ...children) => ({
+    type,
+    props: {
+        ...props,
+        children: children.map(transformChild)
+    }
+});
+
+const createTextElement = text => ({
+    type: 'TEXT_ELEMENT',
+    props: {
+        nodeValue: text,
+        children: []
+    }
+})
+
+const Didact = {
+    createElement
+}
+
+/** @jsx Didact.createElement */
+const element = (
+    <div id="foo">
+        <a>bar</a>
+        <b />
+    </div>
+)
+
+console.log(JSON.stringify(result));
